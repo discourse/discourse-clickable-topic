eb7392ac3b8ca0790570a9f3b963fb6d3fbe25ec
Osama Sayegh <asooomaasoooma90@gmail.com>
2023-10-13 04:32:04 +0300

    DEV: Various improvements and refactoring (#4)
    This commit makes a few improvements to this component such as:
    
    1. Remove patches to the `topic-list-item` and `latest-topic-list-item` core components
    2. Reduce duplicate code
    3. Add various linting/code format config files and lint/format the files

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Discourse Clickable Topic Component
 This component makes the entire area of a `topic-list-item` clickable, as opposed to just the title.
 
-![Discourse Clickable Topic Preivew](readme-assets/preview.gif)
+![Discourse Clickable Topic Preivew](https://github.com/discourse/discourse-clickable-topic/assets/17474474/720acb71-b283-4a3a-ad98-39a0d502ca1e)
 
-[Preview on theme-creator](https://theme-creator.discourse.org/theme/jordan.vidrine/clickable-topic 
-)
+[Preview on theme-creator](https://theme-creator.discourse.org/theme/jordan.vidrine/clickable-topic)
---
+++ javascripts/discourse/connectors/above-latest-topic-list-item/clickable-topic-row.js
@@ -0,0 +1,30 @@
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+import { wantsNewWindow } from "discourse/lib/intercept-click";
+import { navigateToTopic } from "discourse/components/topic-list-item";
+import { bind } from "discourse-common/utils/decorators";
+
+export default class extends Component {
+  @bind
+  clickHandler(event) {
+    const targetElement = event.target;
+    const topic = this.args.outletArgs.topic;
+
+    if (targetElement.tagName === "DIV") {
+      if (wantsNewWindow(event)) {
+        return true;
+      }
+      return navigateToTopic.call(this, topic, topic.lastUnreadUrl);
+    }
+  }
+
+  @action
+  registerClickHandler(element) {
+    element.parentElement.addEventListener("click", this.clickHandler);
+  }
+
+  @action
+  removeClickHandler(element) {
+    element.parentElement.removeEventListener("click", this.clickHandler);
+  }
+}
